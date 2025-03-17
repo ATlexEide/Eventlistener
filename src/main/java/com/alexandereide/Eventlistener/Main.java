48743aa67410ee5c21f5eba663bf31fcee94702c
Alex <atlexeide@gmail.com>
2025-03-17 17:48:11 +0100

    Update plugin

--- src/main/java/com/alexandereide/Eventlistener/Main.java
+++ src/main/java/com/alexandereide/Eventlistener/Main.java
@@ -5,15 +5,29 @@ import org.bukkit.plugin.java.JavaPlugin;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import io.javalin.Javalin;
 
 
 
-public final class Main extends JavaPlugin {
+
+public final class Main extends JavaPlugin  {
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    
+   public void start() {
+   	try {
+		Javalin app = Javalin.create().start("127.0.0.1", 3000);
+		
+	    app.get("/", ctx -> ctx.result("Hello, World!"));
+	    Bukkit.getLogger().info("HTTP SERVER RUNNING");
+	    		
+	} catch (Exception e) {
+		// TODO: handle exception
+	}
+   }
 
 	@Override
 	public void onEnable() {
-
+		start();
         Data serverInfo = new Data();
         serverInfo.ip = Bukkit.getIp();
         serverInfo.port = String.valueOf(Bukkit.getPort());
