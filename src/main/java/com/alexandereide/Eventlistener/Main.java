d68928d80af275dcda74e6418f89fd4f5f5e1b84
Alexander Eide <atlexeide@gmail.com>
2025-05-17 23:24:06 +0200

    Move http server from Main to Server

--- src/main/java/com/alexandereide/Eventlistener/Main.java
+++ src/main/java/com/alexandereide/Eventlistener/Main.java
@@ -6,38 +6,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import io.javalin.Javalin;
+
 
 
 public final class Main extends JavaPlugin  {
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
     
-   public void start() {
-   	try {
-		Javalin app = Javalin.create(config -> {
-            config.bundledPlugins.enableCors(cors -> {
-                cors.addRule(it -> {
-                    it.anyHost();
-                });
-            });
-        }).start("127.0.0.1", 3001);
-	    app.get("/status", ctx -> {
-            String json = gson.toJson(new Data());
-            ctx.result(json);
-        });
 
-        app.post("/chat", ctx -> {
-            StringBuilder sb = new StringBuilder(ctx.body());
-            String resultString = sb.deleteCharAt(0).deleteCharAt(sb.length()-1).toString().replace("\\","");
-            Bukkit.broadcastMessage(resultString);
-            ctx.status();
-        });
-	    Bukkit.getLogger().info("HTTP SERVER RUNNING");
-
-	} catch (Exception e) {
-		// TODO: handle exception
-	}
-   }
 
 	@Override
 	public void onEnable() {
@@ -51,7 +26,7 @@ public final class Main extends JavaPlugin  {
         Bukkit.getPluginManager().registerEvents(new Events(), this);
         String json = gson.toJson(serverInfo);
 
-		start();
+		Server.start("127.0.0.1",3001);
         Server.send(json);
         Bukkit.getLogger().info("Plugin Running OWO");
     }
--- src/main/java/com/alexandereide/Eventlistener/Server.java
+++ src/main/java/com/alexandereide/Eventlistener/Server.java
@@ -1,12 +1,54 @@
 package com.alexandereide.Eventlistener;
+import io.javalin.Javalin;
+import io.javalin.plugin.bundled.CorsPluginConfig;
 import org.bukkit.Bukkit;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.bukkit.command.defaults.BukkitCommand;
+import org.bukkit.scheduler.BukkitRunnable;
+
 
 public class Server {
+	static Gson gson = new GsonBuilder().setPrettyPrinting().create();
+	static void start(String ip, int port){
+		try {
+			Javalin app = Javalin.create(config -> {
+				config.bundledPlugins.enableCors(cors -> {
+					cors.addRule(CorsPluginConfig.CorsRule::anyHost);
+				});
+			}).start(ip, port);
+
+			app.get("/status", ctx -> {
+				String json = gson.toJson(new Data());
+				ctx.result(json);
+			});
+
+			app.post("/chat", ctx -> {
+				StringBuilder sb = new StringBuilder(ctx.body());
+				String resultString = sb.deleteCharAt(0).deleteCharAt(sb.length()-1).toString().replace("\\","");
+				Bukkit.broadcastMessage(resultString);
+				ctx.status();
+			});
+
+			app.post("/cmd", ctx -> {
+				Bukkit.getLogger().info(ctx.body());
+
+				Bukkit.getScheduler().runTaskLater(Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "say hi"));
+				Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "say hi");
+				//Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), ctx.body());
+				ctx.status();
+			});
+			Bukkit.getLogger().info("HTTP SERVER RUNNING");
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
     static void send(String data){
         try {
             Bukkit.getServer().getLogger().info(data);
