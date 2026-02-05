e08fe8060e01aac7c63711c054f05bf829074393
Alexander Eide <atlexeide@gmail.com>
2026-02-05 19:47:52 +0100

    Add fields

--- src/main/java/com/alexandereide/Eventlistener/Data.java
+++ src/main/java/com/alexandereide/Eventlistener/Data.java
@@ -1,13 +1,19 @@
 package com.alexandereide.Eventlistener;
 
+
 public class Data {
 
+		public static String guildId;
+		public static String discordIp;
 	    public static String ip;
 	    public static String port;
 	    public static String version;
 	    public static int onlinePlayers;
 	    public static String motd;
 	    public int playerCount = onlinePlayers;
+
+		public String guild_id = guildId;
+	    public String discord_ip = discordIp;
 	    public String server_message = motd;
 	    public String server_version = version;
 	    public String server_ip = ip;
@@ -21,4 +27,3 @@ public class Data {
 		public String gameMode;
 		public String newGameMode;
 	}
-
